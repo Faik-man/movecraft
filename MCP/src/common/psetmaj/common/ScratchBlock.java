4b04eb1d7abc4bac562b25ec8cac8c5b286d8f47
Sanguivore <Sanguivore@localhost>
2012-10-21 11:20:09 -0600

    Initial Commit, includes Soul Stone

---
+++ MCP/src/common/psetmaj/common/ScratchBlock.java
@@ -0,0 +1,11 @@
+package psetmaj.common;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.Material;
+
+public class ScratchBlock extends Block {
+
+	public ScratchBlock(int id, int textureID) {
+		super(id,textureID, Material.rock);
+	}
+}
